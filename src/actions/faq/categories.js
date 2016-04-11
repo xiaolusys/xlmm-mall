4bb40f7a1c2443f44d7efa947ba22f1c577ea799
Shawn Sit <shawn@xiaolu.so>
2016-04-11 19:35:23 +0800

    faq init

---
+++ src/actions/faq/categories.js
@@ -0,0 +1,4 @@
+import fetch from 'isomorphic-fetch';
+
+export const REQUEST_CATEGORIES = 'REQUEST_CATEGORIES';
+export const RECEIVE_CATEGORIES = 'RECEIVE_CATEGORIES';
--- src/containers/FaqCategory/index.js
+++ src/containers/FaqCategory/index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from 'actions/faq/categories';
 import { Header } from 'components/Header';
 
+@connect(
+  dispatch => bindActionCreators(actionCreators, dispatch),
+)
 export class FaqCategory extends Component {
   static propTypes = {
     children: React.PropTypes.any,
@@ -10,18 +16,11 @@ export class FaqCategory extends Component {
     super(props);
   }
 
-  rightBtnClick = () => {
-
-  };
-
-  leftBtnClick = () => {
-
-  };
-
   render() {
+    const props = this.props;
     return (
       <section>
-        <Header title="常见问题" leftIcon="icon-angle-left" rightIcon="icon-angle-up" />
+        <Header title="常见问题" leftIcon="icon-angle-left" rightIcon="icon-angle-up" leftBtnClick={props.history.goBack} />
         {this.props.children}
       </section>
     );
