ee4ebac5874dc0a7cf9a7d187067f3f95d247d4c
Shawn Sit <shawn@xiaolu.so>
2016-04-13 21:28:16 +0800

    faq list

--- src/containers/FaqList/index.js
+++ src/containers/FaqList/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import _ from 'underscore';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from 'actions/faq/questions';
 import { Header } from 'components/Header';
 import { Footer } from 'components/Footer';
+
+import './index.scss';
 
 @connect(
   state => ({
@@ -20,6 +23,7 @@ export class FaqList extends Component {
     isLoading: React.PropTypes.bool,
     error: React.PropTypes.bool,
     fetchQuestions: React.PropTypes.func,
+    params: React.PropTypes.object,
   };
 
   static defaultProps = {
@@ -31,7 +35,8 @@ export class FaqList extends Component {
   }
 
   componentWillMount() {
-    this.props.fetchQuestions();
+    const { params } = this.props;
+    this.props.fetchQuestions(params.categoryId);
   }
 
   onItemClick = (e) => {
@@ -39,11 +44,31 @@ export class FaqList extends Component {
   };
 
   render() {
-    const props = this.props;
+    const { data } = this.props;
+    const questions = data.results;
     return (
       <div>
-        <Header title="物流问题" leftIcon="icon-angle-left" leftBtnClick={props.history.goBack} />
-        <Footer />
+        <Header title="" leftIcon="icon-angle-left" leftBtnClick={ null } />
+        <div className="has-header content">
+          <ul className="questions-list">
+          <If condition={_.isArray(questions)}>
+          {
+            questions.map((item, index) => {
+              return (
+                <li className="row" key={index}>
+                  <div>
+                    <p className="col-xs-11 font-md">{item.question}</p>
+                    <i className="col-xs-1 icon-angle-down icon-balck"></i>
+                  </div>
+                  <p className="collapse">{item.answer}</p>
+                </li>
+              );
+            })
+          }
+          </If>
+          </ul>
+          <Footer />
+        </div>
       </div>
     );
   }
