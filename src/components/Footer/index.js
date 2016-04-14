756087e47e7bfe6d679bf76dfc606a331680bd7c
Shawn Sit <shawn@xiaolu.so>
2016-04-14 18:22:35 +0800

    add footer styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import './index.scss';
+
 export class Footer extends Component {
   static propTypes = {
     children: React.PropTypes.any,
@@ -7,7 +9,7 @@ export class Footer extends Component {
 
   render() {
     return (
-      <footer className="text-center font-xxs">
+      <footer className="text-center font-xxs footer">
         <p>Copyright © 2014-2015 小鹿美美，All Rights Reserved</p>
         <p> 沪ICP备15013901号-1</p>
       </footer>
