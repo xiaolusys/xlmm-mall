839ce61a8bda2647b2db71b28188b733d7999b6c
Shawn Sit <shawn@xiaolu.so>
2016-04-16 16:40:57 +0800

    add api base

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(morgan('short'));
 app.use(express.static(__dirname + '/'));
 // if API is ready, proxy to api server. else use mock data.
 app.use(mocks());
-app.use('/rest', proxy(url.parse('http://' + config.apiHost + ':' + config.apiPort)));
+app.use('/rest', proxy(url.parse('http://' + config.apiHost + ':' + config.apiPort + '/rest')));
 app.get('/.*/', function root(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
