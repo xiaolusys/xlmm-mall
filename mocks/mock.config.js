0293a8483e3f2aa0a7176a0c2b7977708902afbe
Shawn Sit <xueqingxiao@gmail.com>
2016-04-19 20:52:14 +0800

    disable faq mock data

--- mocks/mock.config.js
+++ mocks/mock.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  '/rest/v1/faqs': {
-    GET: 'data/faqs.get.json',
-  },
-  '/rest/v1/faqs/get_question': {
-    GET: 'data/questions.get.json',
-  },
+  // '/rest/v1/faqs': {
+  //   GET: 'data/faqs.get.json',
+  // },
+  // '/rest/v1/faqs/get_question': {
+  //   GET: 'data/questions.get.json',
+  // },
 };
