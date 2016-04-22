c9b59ea4d501ee4692f00386f4cedb1f8fdccc2c
Shawn Sit <shawn@xiaolu.so>
2016-04-22 16:17:32 +0800

    reorg router

--- src/routes.js
+++ src/routes.js
@@ -10,12 +10,12 @@ import { Login, Register, ResetPassword, UserProfile } from 'containers/User';
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
+    <Route path="/user/profile" component={UserProfile}/>
     <Route path="/user/login" component={Login} />
     <Route path="/user/register" component={Register} />
     <Route path="/user/password/reset" component={ResetPassword} />
     <Route path="faq" component={FaqCategory} />
     <Route path="faq/list/:id/:name" component={FaqList} />
-    <Route path="user/profile" component={UserProfile}/>
     <Route status={404} path="*" component={Home} />
   </Route>
 );
