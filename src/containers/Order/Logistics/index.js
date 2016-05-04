66be3b0370e78b814d11f74e7e31e301a0ee94c9
Shawn Sit <shawn@xiaolu.so>
2016-05-04 10:09:54 +0800

    Logistics & update timeline compontent

--- src/containers/Order/Logistics/index.js
+++ src/containers/Order/Logistics/index.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import { Header } from 'components/Header';
 import { Footer } from 'components/Footer';
+import { Timeline, TimelineItem } from 'components/Timeline';
+import * as actionCreators from 'actions/order/logistics';
 
+import './index.scss';
+
+@connect(
+  state => ({
+    logistics: state.logistics.data,
+    isLoading: state.logistics.isLoading,
+  }),
+  dispatch => bindActionCreators(actionCreators, dispatch),
+)
 export default class Logistics extends Component {
   static propTypes = {
-    children: React.PropTypes.any,
+    params: React.PropTypes.any,
+    logistics: React.PropTypes.object,
+    isLoading: React.PropTypes.bool,
+    fetchLogistics: React.PropTypes.func,
   };
 
   static contextTypes = {
@@ -16,11 +32,31 @@ export default class Logistics extends Component {
     context.router;
   }
 
+  componentWillMount() {
+    this.props.fetchLogistics(this.props.params.id);
+  }
+
   render() {
+    const { logistics } = this.props || {};
+    const logisticsInfo = logistics.data || [];
     return (
       <div>
         <Header title="物流信息" leftIcon="icon-angle-left" onLeftBtnClick={this.context.router.goBack} />
           <div className="content has-header">
+            <p className="logistics-item bottom-border"><span>快递公司</span><span className="pull-right">{logistics.name}</span></p>
+            <p className="logistics-item bottom-border"><span>快递单号</span><span className="pull-right">{logistics.order}</span></p>
+            <div className="logistics-item margin-top-xs">
+              <Timeline className="logistics-info">
+              {logisticsInfo.map((item, index) => {
+                return (
+                  <TimelineItem headColor="grey" tailColor="grey">
+                    <p className="font-grey">{item.time.replace('T', ' ')}</p>
+                    <p className="font-sm">{item.content}</p>
+                  </TimelineItem>
+                );
+              })}
+              </Timeline>
+            </div>
             <Footer />
           </div>
       </div>
