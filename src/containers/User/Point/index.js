f924bee5e4efe7b32c15125aef145a54c1c4aaa8
Brook <brook.zhao@xiaolumeimei.com>
2016-05-05 18:51:15 +0800

    fix

--- src/containers/User/Point/index.js
+++ src/containers/User/Point/index.js
@@ -5,9 +5,7 @@ import classnames from 'classnames';
 import { connect } from 'react-redux';
 import { Header } from 'components/Header';
 import { Footer } from 'components/Footer';
-import { Input } from 'components/Input';
 import * as actionCreators from 'actions/user/point';
-import { Toast } from 'components/Toast';
 
 import './index.scss';
 
@@ -24,10 +22,10 @@ import './index.scss';
 export default class Point extends Component {
   static propTypes = {
     children: React.PropTypes.array,
-    point: React.PropTypes.any,
     dispatch: React.PropTypes.func,
     isLoading: React.PropTypes.bool,
     error: React.PropTypes.bool,
+    point: React.PropTypes.any,
     fetchPoint: React.PropTypes.func,
   };
 
@@ -53,12 +51,15 @@ export default class Point extends Component {
   }
 
   render() {
-      return (
-          <div>
+    const logoutBtnCls = classnames({
+      ['col-xs-4 col-xs-offset-4 margin-top-xs margin-bottom-xs button button-energized']: 1,
+    });
+    return (
+      <div>
         <Header title="我的积分" leftIcon="icon-angle-left" onLeftBtnClick={this.context.router.goBack} rightText={'使用说明'} onRightBtnClick={this.onInstructionsClick} />
         <div className="has-header content point-container">
           <div className="row my-point padding-bottom-xxs">
-            <p className="text-center no-margin">25</p>
+            <p className="text-center no-margin">96</p>
             <span className="col-xs-12 text-center">我的积分</span>
           </div>
           <ul className="point-list">
@@ -94,31 +95,13 @@ export default class Point extends Component {
                 <p className="col-xs-12">订单号 9862453</p>
               </div>
             </li>
-            <li className="row no-margin bottom-border">
-              <div className="col-xs-12 padding-top-xxs">
-                <p className="col-xs-12">2015-11-03 16:45:45</p>
-                <p className="col-xs-9">购物订单完成建立积分</p>
-                <span className="col-xs-3">+24分</span>
-                <p className="col-xs-12">订单号 9862453</p>
-              </div>
-            </li>
-            <li className="row no-margin bottom-border">
-              <div className="col-xs-12 padding-top-xxs">
-                <p className="col-xs-12">2015-11-03 16:45:45</p>
-                <p className="col-xs-9">购物订单完成建立积分</p>
-                <span className="col-xs-3">+24分</span>
-                <p className="col-xs-12">订单号 9862453</p>
-              </div>
-            </li>
-            <li className="row no-margin bottom-border">
-              <div className="col-xs-12 padding-top-xxs">
-                <p className="col-xs-12">2015-11-03 16:45:45</p>
-                <p className="col-xs-9">购物订单完成建立积分</p>
-                <span className="col-xs-3">+24分</span>
-                <p className="col-xs-12">订单号 9862453</p>
-              </div>
-            </li>
           </ul>
+          <div className="text-center">
+            <i className="icon-database icon-5x"/>
+            <p>您暂时还没有积分纪录哦～</p>
+            <p className="font-xs font-grey-light">快去下单赚取积分吧～</p>
+            <button className={logoutBtnCls} type="button" onClick={this.onLogoutBtnClick}>快去抢购</button>
+          </div>
           <Footer/>
         </div>
       </div>
