f508acadfd98fdd8bceab92cf0479387428c4bc6
Shawn Sit <shawn@xiaolu.so>
2016-05-10 13:42:05 +0800

    add constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,6 @@
 export const baseEndpoint = '/rest/v2/';
 export const baseEndpointV1 = '/rest/v1/';
 export const offset = 250;
-export const routesPrefix = '/mall/';
+export const image = {
+  square: '?imageMogr2/thumbnail/200/format/jpg/quality/90/crop/200x200',
+};
