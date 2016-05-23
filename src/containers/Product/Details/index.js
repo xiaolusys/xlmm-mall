4ac4f6a369843ac9105664b962828384052a7cb8
Shawn Sit <shawn@xiaolu.so>
2016-05-23 10:12:38 +0800

    add alert

--- src/containers/Product/Details/index.js
+++ src/containers/Product/Details/index.js
@@ -153,6 +153,15 @@ export default class Detail extends Component {
 
   onShareBtnClick = (e) => {
     const shareInfo = this.props.share.data;
+    alert({
+      method: 'callNativeUniShareFunc',
+      data: {
+        share_desc: shareInfo.desc,
+        share_icon: shareInfo.share_img,
+        share_type: 'link',
+        link: shareInfo.share_link,
+      },
+    });
     plugins.invoke({
       method: 'callNativeUniShareFunc',
       data: {
