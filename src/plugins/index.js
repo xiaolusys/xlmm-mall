dc6321053532adb435f921377964d11f0b611a5c
Shawn Sit <shawn@xiaolu.so>
2016-05-23 18:36:32 +0800

    remove alert

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,4 +1,4 @@
-import * as utils from 'utils';
+alertimport * as utils from 'utils';
 import _ from 'underscore';
 
 const setupWebViewJavascriptBridge = (callback) => {
@@ -23,7 +23,6 @@ export const invoke = (params) => {
     });
   } else if (utils.detector.isApp() && utils.detector.isAndroid()) {
     if (params.data) {
-      alert(JSON.stringify(params.data));
       window.AndroidBridge[params.method](JSON.stringify(params.data));
     }
     window.AndroidBridge[params.method]();
