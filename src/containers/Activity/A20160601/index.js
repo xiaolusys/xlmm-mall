8799552f8eee4ed149578f5f94812c7be3b2fcfd
Brook <brook.zhao@xiaolumeimei.com>
2016-05-28 19:26:06 +0800

    test

--- src/containers/Activity/A20160601/index.js
+++ src/containers/Activity/A20160601/index.js
@@ -81,14 +81,6 @@ export default class A20160601 extends Component {
     const appUrl = 'com.jimei.xlmm://app/v1/products/modelist?model_id=' + modelId;
     const appVersion = window.AndroidBridge.appVersion();
     console.log(appVersion);
-    if (utils.detector.isApp()) {
-      alert('app');
-      plugins.invoke({
-        method: 'jumpToNativeLocation',
-        data: { target_url: 'com.jimei.xlmm://app/v1/products?product_id=' + window.location.href.substring(0, window.location.href.indexOf('#')) + '#/product/details/' + modelId },
-      });
-      return;
-    }
     if (utils.detector.isAndroid()) {
       if (Number(appVersion) >= 20160528) {
         plugins.invoke({
