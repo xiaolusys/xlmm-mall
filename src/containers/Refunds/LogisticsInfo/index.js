c529d1e408318126b8ff395471368a97da1c6c25
Brook <brook.zhao@xiaolumeimei.com>
2016-05-28 16:05:45 +0800

    fix

--- src/containers/Refunds/LogisticsInfo/index.js
+++ src/containers/Refunds/LogisticsInfo/index.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
 
 export default class LogisticsInfo extends Component {
-  static PropTypes = {
-    children: React.PropTypes.array,
-    data: React.PropTypes.any,
-    params: React.PropTypes.object,
-    dispatch: React.PropTypes.func,
-    isLoading: React.PropTypes.bool,
-    error: React.PropTypes.bool,
-  };
-
   static contextTypes = {
     router: React.PropTypes.object,
   };
