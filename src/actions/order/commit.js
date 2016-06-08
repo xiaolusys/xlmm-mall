9e973007aedef6c9ec3392a9d0b943c474824700
Shawn Sit <shawn@xiaolu.so>
2016-06-08 09:38:56 +0800

    optimize ga

--- src/actions/order/commit.js
+++ src/actions/order/commit.js
@@ -11,12 +11,6 @@ export const commitOrder = (params) => {
     hitType: 'event',
     eventCategory: 'Pay',
     eventAction: 'Request',
-    eventLabel: 'All',
-  });
-  window.ga && window.ga('send', {
-    hitType: 'event',
-    eventCategory: 'Pay',
-    eventAction: 'Request',
     eventLabel: constants.gaPayTypes[params.channel],
   });
   return (dispatch) => {
