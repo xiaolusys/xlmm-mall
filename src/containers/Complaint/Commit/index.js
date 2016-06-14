33bc27da953cafb947da61a239342bd3cf64c8ae
Brook <brook.zhao@xiaolumeimei.com>
2016-06-14 17:08:33 +0800

    code format

--- src/containers/Complaint/Commit/index.js
+++ src/containers/Complaint/Commit/index.js
@@ -82,9 +82,6 @@ export default class Commit extends Component {
   }
 
   render() {
-    const bindPhoneBtnCls = classnames({
-      ['col-xs-10 col-xs-offset-1 margin-top-xs button button-energized']: 1,
-    });
     return (
       <div>
         <Header title="投诉建议" leftIcon="icon-angle-left" onLeftBtnClick={this.context.router.goBack} rightText="历史记录" onRightBtnClick={this.onRightBtnClick} />
@@ -92,7 +89,7 @@ export default class Commit extends Component {
           <textarea placeholder="请输入您的意见和建议，以便我们更好地服务于您!" value={this.state.textareaContent} onChange={this.onTextareaChange}></textarea>
           <p className="col-xs-12 text-right no-margin font-grey-light text-range">{this.state.textRange}</p>
           <div className="row no-margin">
-            <button className={bindPhoneBtnCls} type="button" onClick={this.onBubmitBtnClick} disabled={this.state.save}>提交</button>
+            <button className="col-xs-10 col-xs-offset-1 margin-top-xs button button-energized" type="button" onClick={this.onBubmitBtnClick} disabled={this.state.save}>提交</button>
           </div>
           <Footer/>
         </div>
