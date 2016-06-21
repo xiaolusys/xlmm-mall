e42b942ed408ece472315767133a1e302b88ca24
Shawn Sit <shawn@xiaolu.so>
2016-06-21 15:36:40 +0800

    disable debug

--- src/utils/wechat.js
+++ src/utils/wechat.js
@@ -10,7 +10,7 @@ class WechatUtils {
 
   config(params) {
     window.wx.config({
-      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
       appId: params.app_id, // 必填，公众号的唯一标识
       timestamp: params.timestamp, // 必填，生成签名的时间戳
       nonceStr: params.noncestr, // 必填，生成签名的随机串
