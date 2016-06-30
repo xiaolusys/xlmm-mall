c66ffcd210b5c5a87d9d1cc6de7b847a7eeb2d23
Shawn Sit <shawn@xiaolu.so>
2016-06-30 18:54:14 +0800

    activity 20160628

--- src/containers/Activity/index.js
+++ src/containers/Activity/index.js
@@ -1,6 +1,6 @@
 export A20160625 from './A20160625';
 export A20160621 from './A20160621';
-export A20160628 from './A20160628';
+export A20160701 from './A20160701';
 export ExamHome from './Exam/Home';
 export ExamQuestion from './Exam/Question';
 export ExamResult from './Exam/Result';
