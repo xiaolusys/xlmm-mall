976012992b7b485f86c4343420d98aa2e811e2ce
Brook <brook.zhao@xiaolumeimei.com>
2016-07-01 18:23:41 +0800

    add activity model

--- src/actions/user/coupon.js
+++ src/actions/user/coupon.js
@@ -2,17 +2,19 @@ import * as constants from 'constants';
 import axios from 'axios';
 import qs from 'qs';
 import createAction from '../createAction';
+import activityModelAction from '../activity/model';
 
 export const name = 'FETCH_COUPON';
 
 const action = createAction(name);
 
-export const receiveCoupon = (couponIds) => {
+export const receiveCoupon = (couponIds, activityId) => {
   return (dispatch) => {
     dispatch(action.request());
     return axios.post(constants.baseEndpointV1 + 'usercoupons', qs.stringify({ template_id: couponIds }))
       .then((resp) => {
         dispatch(action.success(resp.data));
+        dispatch(activityModelAction.fetchModel(activityId));
       })
       .catch((resp) => {
         dispatch(action.failure(resp.data));
