fa79db327dd805e017250ed6f72b458b3fc8cf9a
Brook <brook.zhao@xiaolumeimei.com>
2016-07-07 14:46:31 +0800

    fix

--- src/actions/user/coupon.js
+++ src/actions/user/coupon.js
@@ -41,16 +41,3 @@ export const resetCoupon = () => {
     dispatch(action.reset());
   };
 };
-
-export const verifyCoupon = (cartIds, couponId) => {
-  return (dispatch) => {
-    dispatch(action.request());
-    return axios.get(constants.baseEndpointV1 + 'carts/carts_payinfo', { params: { cart_ids: cartIds, coupon_id: couponId } })
-      .then((resp) => {
-        dispatch(action.success(resp.data));
-      })
-      .catch((resp) => {
-        dispatch(action.failure(resp.data));
-      });
-  };
-};
