095613d264551e1645af022d3aedce873dd48305
Shawn Sit <shawn@xiaolu.so>
2016-07-16 15:07:48 +0800

    enhancement

--- src/containers/Activity/ShopOpeningSucceed/index.js
+++ src/containers/Activity/ShopOpeningSucceed/index.js
@@ -6,14 +6,28 @@ import _ from 'underscore';
 import { Image } from 'components/Image';
 import { Checkbox } from 'components/Checkbox';
 import { Input } from 'components/Input';
+import { WechatPopup } from 'components/WechatPopup';
+import * as utils from 'utils';
+import * as inviteSharingAction from 'actions/activity/inviteSharing';
+import * as wechatSignAction from 'actions/wechat/sign';
 
 // import './index.scss';
-
+const actionCreators = _.extend(inviteSharingAction, wechatSignAction);
 const banner = 'http://7xogkj.com1.z0.glb.clouddn.com/mall/opening-shop-succeed.png';
-
+@connect(
+  state => ({
+    inviteSharing: state.inviteSharing,
+    wechatSign: state.wechatSign,
+  }),
+  dispatch => bindActionCreators(actionCreators, dispatch),
+)
 export default class ShopOpeningSucceed extends Component {
   static propTypes = {
     children: React.PropTypes.array,
+    fetchInviteSharing: React.PropTypes.func,
+    fetchWechatSign: React.PropTypes.func,
+    inviteSharing: React.PropTypes.object,
+    wechatSign: React.PropTypes.object,
   };
 
   static contextTypes = {
@@ -25,16 +39,51 @@ export default class ShopOpeningSucceed extends Component {
     context.router;
   }
 
-  componentWillMount() {
+  state = {
+    sharePopupActive: false,
+  }
 
+  componentWillMount() {
+    this.props.fetchWechatSign();
+    this.props.fetchInviteSharing(27);
   }
 
   componentDidMount() {
     document.body.classList.add('content-white-bg');
   }
 
+  componentWillReceiveProps(nextProps) {
+    const { mamaCharge, wechatSign, inviteSharing } = nextProps;
+    utils.wechat.config(wechatSign);
+
+    if (!inviteSharing.isLoading && inviteSharing.success) {
+      const shareInfo = {
+        success: inviteSharing.success,
+        data: {
+          title: inviteSharing.data.title,
+          desc: inviteSharing.data.active_dec,
+          share_link: inviteSharing.data.share_link,
+          share_img: inviteSharing.data.share_icon,
+        },
+      };
+      utils.wechat.configShareContent(shareInfo);
+    }
+
+  }
+
   componentWillUnmount() {
     document.body.classList.remove('content-white-bg');
+  }
+
+  onShareBtnClick = (e) => {
+    if (utils.detector.isWechat()) {
+      this.setState({ sharePopupActive: true });
+      return;
+    }
+  }
+
+  onCloseBtnClick = (e) => {
+    this.setState({ sharePopupActive: false });
   }
 
   render() {
@@ -42,8 +91,9 @@ export default class ShopOpeningSucceed extends Component {
       <div className="col-xs-12 col-sm-8 col-sm-offset-2 no-padding opening-shop">
         <Image style={{ width: '100%' }} src={banner} />
         <div className="row no-margin text-center margin-bottom-xs">
-          <button className="col-xs-10 col-xs-offset-1 button button-energized">邀请好友一元开店</button>
+          <button className="col-xs-10 col-xs-offset-1 button button-energized" onClick={this.onShareBtnClick}>邀请好友一元开店</button>
         </div>
+        <WechatPopup active={this.state.sharePopupActive} onCloseBtnClick={this.onCloseBtnClick}/>
       </div>
     );
   }
