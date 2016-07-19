084d6d7c5cc8cfc5143d9d8e7e2f6f7c0e5137d8
Brook <brook.zhao@xiaolumeimei.com>
2016-07-19 10:46:25 +0800

    add fensi id

--- src/actions/user/coupons.js
+++ src/actions/user/coupons.js
@@ -3,16 +3,14 @@ import axios from 'axios';
 import qs from 'qs';
 import _ from 'underscore';
 import createAction from '../createAction';
-
 export const name = 'FETCH_COUPONS_BY_STATUS';
-
 const action = createAction(name);
 export const fetchCouponsByStatus = (status) => {
   return (dispatch) => {
     dispatch(action.request());
     return axios.get(constants.baseEndpointV1 + 'usercoupons/get_user_coupons?status=' + status)
       .then((resp) => {
-        const data = {};
+        const data = resp.data;
         data.status = status;
         data.coupons = resp.data;
         dispatch(action.success(data));
