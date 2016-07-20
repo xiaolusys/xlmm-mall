05c9cbab7a47f8e8102c5d14e3a18e2eeac7784e
Shawn Sit <shawn@xiaolu.so>
2016-07-20 20:28:09 +0800

    bugfixs

--- src/reducers/verifyCodeReducer.js
+++ src/reducers/verifyCodeReducer.js
@@ -25,12 +25,16 @@ export default (state = initState, action = null) => {
       return _.extend({}, state, { fetch: { isLoading: false, data: action.payload, success: true, error: false } });
     case verifyCodeAction.names.FETCH_VERIFY_CODE + '_' + actionTypes.FAILURE:
       return _.extend({}, state, { fetch: { isLoading: false, data: action.payload, success: false, error: true, status: action.status } });
+    case verifyCodeAction.names.FETCH_VERIFY_CODE + '_' + actionTypes.RESET:
+      return _.extend({}, state, { verify: { isLoading: false, data: {}, success: false, error: false, status: action.status } });
     case verifyCodeAction.names.VERIFY + '_' + actionTypes.REQUEST:
       return _.extend({}, state, { verify: { isLoading: true, success: false, error: false } });
     case verifyCodeAction.names.VERIFY + '_' + actionTypes.SUCCESS:
       return _.extend({}, state, { verify: { isLoading: false, data: action.payload, success: true, error: false } });
     case verifyCodeAction.names.VERIFY + '_' + actionTypes.FAILURE:
       return _.extend({}, state, { verify: { isLoading: false, data: action.payload, success: false, error: true, status: action.status } });
+    case verifyCodeAction.names.VERIFY + '_' + actionTypes.RESET:
+      return _.extend({}, state, { verify: { isLoading: false, data: {}, success: false, error: false, status: action.status } });
     default:
       return state;
   }
