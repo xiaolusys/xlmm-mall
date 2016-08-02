1f350106d713c20587476bc5495b3c18ab76b953
Brook <brook.zhao@xiaolumeimei.com>
2016-08-02 16:49:14 +0800

    src/containers/Order/Commit/index.js

--- src/containers/Order/Commit/index.js
+++ src/containers/Order/Commit/index.js
@@ -265,6 +265,9 @@ export default class Commit extends Component {
     }
     if (displayPrice > 0 && coupon.success && coupon.data.status === 0 && payInfo.data.total_fee >= coupon.data.use_fee) {
       displayPrice = displayPrice - coupon.data.coupon_value;
+      if (displayPrice < 0) {
+        displayPrice = 0;
+      }
     }
     return displayPrice.toFixed(2);
   }
