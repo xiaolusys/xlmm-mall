41821537e4170541882b343b1c30b8f34760d15b
Brook <brook.zhao@xiaolumeimei.com>
2016-08-05 10:40:56 +0800

    rename style classname

--- src/containers/Product/Details/index.js
+++ src/containers/Product/Details/index.js
@@ -353,7 +353,7 @@ export default class Detail extends Component {
         <div className="product-info bottom-border bg-white">
           <div className="row no-margin">
             <p className="col-xs-8 no-padding no-wrap font-md">{info.name}</p>
-            <div className="col-xs-4 no-padding favorite" onClick={this.onFavoriteBtnClick}>
+            <div className="col-xs-4 no-padding icon-favorite" onClick={this.onFavoriteBtnClick}>
               <If condition={favoriteStatus}>
                 <i className="col-xs-3 icon-favorite-yes font-lg text-left"></i>
               </If>
