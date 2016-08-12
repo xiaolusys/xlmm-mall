31f1a5d4f52166c251b1c1655406fd1b93e6381b
Brook <brook.zhao@xiaolumeimei.com>
2016-08-12 17:19:23 +0800

    change mamaId to mm_linkid in Home page

--- src/containers/Refunds/Detail/index.js
+++ src/containers/Refunds/Detail/index.js
@@ -11,7 +11,9 @@ import { Timeline, TimelineItem } from 'components/Timeline';
 import { Statusline, StatuslineItem } from 'components/Statusline';
 import * as utils from 'utils';
 import * as actionCreators from 'actions/refunds/detail';
+
 import './index.scss';
+
 @connect(
   state => ({
     data: state.refundsDetails.data,
@@ -31,36 +33,48 @@ export default class Detail extends Component {
     error: React.PropTypes.bool,
     fetchRefundsDetail: React.PropTypes.func,
   };
+
   static contextTypes = {
     router: React.PropTypes.object,
   };
+
   constructor(props, context) {
     super(props);
     context.router;
   }
+
   state = {
     refundsInfoIsShow: false,
   }
+
   componentWillMount() {
     this.props.fetchRefundsDetail(this.props.params.refundsid);
   }
+
   onRefundsInfoBtnClick = (e) => {
     this.setState({ refundsInfoIsShow: true });
     e.preventDefault();
   }
+
   onExpressBtnClick = (e) => {
     const { refundsid, orderid } = e.currentTarget.dataset;
     this.context.router.push(`/refunds/express/order/${refundsid}/${orderid}/${encodeURIComponent('请选择物流公司')}`);
     e.preventDefault();
   }
+
   toggleRefundsInfoIsShowState = (e) => {
     this.setState({ refundsInfoIsShow: false });
     e.preventDefault();
   }
+
   render() {
     const { isLoading, data } = this.props;
     let refundStatusList = {};
+    let statusList = [];
     let statuslineWidth = '';
+    if (!_.isEmpty(data.status_shaft)) {
+      statusList = data.status_shaft.reverse();
+    }
     if (data && data.has_good_return) {
       refundStatusList = {
         3: { display: '申请退货' },
@@ -132,15 +146,22 @@ export default class Detail extends Component {
               </Statusline>
             </div>
           </If>
-          <div className="refunds-details-list">
-            <div className="row no-margin bottom-border">
-              <p className="col-xs-12 no-margin text-left">
-                <span className="col-xs-3 no-padding text-left">退款编号</span>
-                <span className="col-xs-9 no-padding no-wrap font-grey-light">{data.refund_no}</span>
+          <ul className="refunds-details-list">
+            <li className="row col-xs-12 no-margin bottom-border">
+              <p className="col-xs-9 no-margin no-padding text-left">
+                <span className="col-xs-5 text-left">退款编号</span>
+                <span className="col-xs-7 no-padding no-wrap font-grey-light">{data.refund_no}</span>
               </p>
-            </div>
+              <p className="col-xs-3 no-margin no-padding text-right font-orange">
+                <span>{data.status_display}</span>
+              </p>
+            </li>
+            <p className="row col-xs-12 no-margin bottom-border text-right font-grey-light font-xs refunds-created">
+              <span>下单时间: </span>
+              <span>{data.created && data.created.replace('T', ' ') }</span>
+            </p>
             <If condition={data.status === 4}>
-              <div className="row no-margin margin-bottom-xs padding-right-xs padding-top-xxs padding-bottom-xxs bottom-border">
+              <li className="row col-xs-12 no-margin margin-bottom-xs padding-right-xs padding-top-xxs padding-bottom-xxs bottom-border">
                 <div className="col-xs-8">
                   <p className="no-wrap no-margin">
                     <span className="margin-right-xxs">{data.return_address.split('，')[2]}</span>
@@ -149,44 +170,60 @@ export default class Detail extends Component {
                   <p className="no-wrap no-margin font-grey-light">{data.return_address.split('，')[0]}</p>
                 </div>
                 <button className="margin-top-xxs button button-light button-sm pull-right" type="button" data-orderid={data.order_id} data-refundsid={this.props.params.refundsid} onClick={this.onExpressBtnClick}>填写快递单</button>
-              </div>
+              </li>
             </If>
-            <div className="row no-margin bottom-border">
+            <li className="row col-xs-12 no-margin bottom-border">
               <div className="col-xs-3">
-                <Image className="login-banner border" thumbnail={60} crop={60 + 'x' + 60} quality={100} src={data.pic_path}/>
+                <Image className="login-banner border" thumbnail={70} crop={70 + 'x' + 70} quality={100} src={data.pic_path}/>
               </div>
               <div className="col-xs-9">
-                <p className="row no-margin padding-top-xxs padding-bottom-xxs">
-                  <span className="col-xs-12 no-padding no-wrap">{data.title}</span>
+                <p className="row no-padding padding-top-xxs padding-bottom-xxs">
+                  <span className="col-xs-9 no-wrap">{data.title}</span>
+                  <span className="col-xs-3 no-padding text-right">{data.total_fee}</span>
                 </p>
-                <p className="row no-margin font-grey-light">
-                  <span className="col-xs-8 no-padding no-wrap">尺码: {data.sku_name}</span>
-                  <span className="padding-left-xs">{'¥' + data.total_fee + ' x' + data.refund_num}</span>
+                <p className="row no-margin no-padding font-grey-light">
+                  <span className="margin-right-xs">尺码: {data.sku_name}</span>
+                  <span className=""></span>
                 </p>
               </div>
-            </div>
-            <div className="row no-margin bottom-border">
+            </li>
+            <li className="row col-xs-12 no-margin bottom-border">
               <p className="col-xs-8 no-margin">申请数量</p>
-              <p className="col-xs-4 no-margin text-right font-grey-light">{data.refund_num}</p>
-            </div>
-            <div className="row no-margin bottom-border">
-              <p className="col-xs-4 no-margin">可退金额</p>
-              <p className="col-xs-8 no-margin text-right font-orange">¥{data.refund_fee}</p>
-            </div>
-            <div className="row no-margin bottom-border">
+              <p className="col-xs-4 no-margin no-padding text-right font-grey-light">{data.refund_num}</p>
+            </li>
+            <li className="row col-xs-12 no-margin bottom-border">
+              <p className="col-xs-4 no-margin">退款金额</p>
+              <p className="col-xs-8 no-margin no-padding text-right">¥{data.refund_fee}</p>
+            </li>
+            <li className="row col-xs-12 no-margin margin-top-xs bottom-border">
               <p className="col-xs-4 no-margin">退款原因</p>
-              <p className="col-xs-8 no-margin text-right font-grey-light">{data.reason}</p>
-            </div>
-            <If condition={!_.isEmpty(data.proof_pic)}>
-              <div className="row no-margin bottom-border">
+              <p className="col-xs-8 no-margin no-padding text-right font-grey-light">{data.reason}</p>
+            </li>
+            <li className="row col-xs-12 no-margin bottom-border">
+              <p className="col-xs-12 no-margin margin-bottom-xxs">{data.desc}</p>
+              <If condition={!_.isEmpty(data.proof_pic)}>
                 {data.proof_pic.map((item, index) => {
                   return (
                     <Image className="login-banner border margin-left-xs margin-bottom-xxs" thumbnail={60} crop={60 + 'x' + 60} quality={100} src={item}/>
                   );
                 })}
-              </div>
+              </If>
+            </li>
+            <If condition={!_.isEmpty(statusList)}>
+              <li className="row col-xs-12 no-margin margin-top-xs padding-left-xs bottom-border">
+              <Timeline className="logistics-info margin-left-xxs">
+                {statusList.map((item, index) => {
+                  return (
+                    <TimelineItem key={index} headColor="grey" tailColor="grey">
+                      <p className="font-grey">{item.time.replace('T', ' ')}</p>
+                      <p className="font-sm">{item.status_display}</p>
+                    </TimelineItem>
+                  );
+                })}
+                </Timeline>
+              </li>
             </If>
-          </div>
+          </ul>
         </div>
       </div>
     );
