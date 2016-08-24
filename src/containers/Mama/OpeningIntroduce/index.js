7b1d1512b5b24604385d9ffd9f980890fa7b94be
Brook <brook.zhao@xiaolumeimei.com>
2016-08-24 17:31:01 +0800

    add banner in mama open introduce page

--- src/containers/Mama/OpeningIntroduce/index.js
+++ src/containers/Mama/OpeningIntroduce/index.js
@@ -30,13 +30,16 @@ export default class OpeningIntroduce extends Component {
       <div>
         <Header title="开店介绍" leftIcon="icon-angle-left" onLeftBtnClick={this.context.router.goBack} />
           <div className="content open-introduce-container">
-            <Image src={`http://7xogkj.com1.z0.glb.clouddn.com//mall/mama/open/introduce.jpg`}/>
-            <Link className="row no-margin" to={`/mct.html`}>
-              <button className="col-xs-10 col-xs-offset-1 margin-top-xs button button-energized" type="button">1元体验15天</button>
-            </Link>
-            <Link className="row no-margin" to={`/mcf.html`}>
-              <button className="col-xs-10 col-xs-offset-1 margin-top-xs button button-energized" type="button">成为正式会员</button>
-            </Link>
+            <Image src={`http://7xogkj.com1.z0.glb.clouddn.com//mall/mama/open/v2/banner.jpg`}/>
+            <Image src={`http://7xogkj.com1.z0.glb.clouddn.com//mall/mama/open/v2/introduce.png`}/>
+            <div className="content-white-bg">
+              <Link className="row no-margin" to={`/mct.html`}>
+                <button className="col-xs-10 col-xs-offset-1 margin-top-xs button button-energized" type="button">1元体验15天</button>
+              </Link>
+              <Link className="row no-margin" to={`/mcf.html`}>
+                <button className="col-xs-10 col-xs-offset-1 margin-top-xs margin-bottom-xs button button-energized" type="button">成为正式会员</button>
+              </Link>
+            </div>
           </div>
       </div>
     );
