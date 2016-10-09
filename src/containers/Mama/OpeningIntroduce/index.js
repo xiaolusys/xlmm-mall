89ec4d2448aa86ff84aa282bb9fa51ead5a28fb9
Zifei Zhong <zfz@xiaolumeimei.com>
2016-10-09 11:02:47 +0800

    remove a button

--- src/containers/Mama/OpeningIntroduce/index.js
+++ src/containers/Mama/OpeningIntroduce/index.js
@@ -23,10 +23,6 @@ export default class OpeningIntroduce extends Component {
     const mamaLinkId = utils.cookie.getCookie('mm_linkid');
     const { type } = e.currentTarget.dataset;
     const { protocol, host } = window.location;
-    if (Number(type) === 1) {
-      window.location.href = `${protocol}//${host}/rest/v1/users/weixin_login/?next=/mall/open/zeroopen?mama_id=${mamaLinkId}`;
-      return;
-    }
     window.location.href = `${protocol}//${host}/rest/v1/users/weixin_login/?next=/mall/mcf.html?mama_id=${mamaLinkId}`;
   }
 
@@ -38,9 +34,6 @@ export default class OpeningIntroduce extends Component {
             <Image src={`http://7xogkj.com1.z0.glb.clouddn.com//mall/mama/open/v2/banner.jpg`}/>
             <Image src={`http://7xogkj.com1.z0.glb.clouddn.com//mall/mama/open/v2/introduce.png`}/>
             <div className="content-white-bg">
-              <div className="row no-margin">
-                <button className="col-xs-10 col-xs-offset-1 margin-top-xs button button-energized" type="button" data-type="1" onClick={this.onBtnClick}>0元开店</button>
-              </div>
               <div className="row no-margin">
                 <button className="col-xs-10 col-xs-offset-1 margin-top-xs margin-bottom-xs button button-energized" type="button" data-type="2" onClick={this.onBtnClick}>成为正式会员</button>
               </div>
