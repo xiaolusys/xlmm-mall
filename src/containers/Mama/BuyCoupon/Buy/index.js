8f67d84231bd37a5b4480db45d340df0121d5fe3
wulei <crackwl@163.com>
2016-11-01 16:37:42 +0800

    coupon buy charge chg

--- src/containers/Mama/BuyCoupon/Buy/index.js
+++ src/containers/Mama/BuyCoupon/Buy/index.js
@@ -241,18 +241,18 @@ export default class BuyCoupon extends Component {
     let payInfo = {};
     if (!_.isEmpty(this.props.payInfo.data)) {
       payInfo = this.props.payInfo.data;
-      /* payInfo.channels = [];
-        payInfo.channels.push({
-          id: 'wx',
-          icon: 'icon-wechat-pay icon-wechat-green',
-          name: '微信支付',
-        });
+      payInfo.channels = [];
+      payInfo.channels.push({
+        id: 'wx',
+        icon: 'icon-wechat-pay icon-wechat-green',
+        name: '微信支付',
+      });
 
-        payInfo.channels.push({
-          id: 'alipay',
-          icon: 'icon-alipay-square icon-alipay-blue',
-          name: '支付宝',
-        });*/
+      payInfo.channels.push({
+        id: 'alipay',
+        icon: 'icon-alipay-square icon-alipay-blue',
+        name: '支付宝',
+      });
     }
     return payInfo;
   }
