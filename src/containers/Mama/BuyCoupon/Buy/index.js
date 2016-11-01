dce599833d9ec612a87be4f6162196b4e7b0a44d
wulei <crackwl@163.com>
2016-11-01 12:09:26 +0800

    hide header in app

--- src/containers/Mama/BuyCoupon/Buy/index.js
+++ src/containers/Mama/BuyCoupon/Buy/index.js
@@ -276,7 +276,7 @@ export default class BuyCoupon extends Component {
     const sku = this.state.sku ? this.state.sku : null;
     return (
       <div className="col-xs-12 col-sm-8 col-sm-offset-2 no-padding content-white-bg buycoupon">
-        <Header title="入券" leftIcon="icon-angle-left" onLeftBtnClick={this.context.router.goBack} />
+        <Header title="入券" leftIcon="icon-angle-left" onLeftBtnClick={this.context.router.goBack} hide={utils.detector.isApp()}/>
         <Image className="coupon-img" src={imgSrc} quality={70} />
         <div className="product-info bottom-border bg-white col-xs-offset-1">
           <div className="row no-margin">
