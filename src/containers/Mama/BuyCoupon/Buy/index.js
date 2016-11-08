383d4f8fe9490413a45d0394d9216839ac79f2b8
wulei <crackwl@163.com>
2016-11-08 15:45:18 +0800

    add elitemama link

--- src/containers/Mama/BuyCoupon/Buy/index.js
+++ src/containers/Mama/BuyCoupon/Buy/index.js
@@ -339,9 +339,11 @@ export default class BuyCoupon extends Component {
     const imgSrc = (this.state.productDetail && this.state.productDetail.detail_content) ? this.state.productDetail.detail_content.head_img : '';
     const payInfo = this.payInfo();
     const sku = this.state.sku ? this.state.sku : null;
+    const trasparentHeader = true;
+
     return (
       <div className="col-xs-12 col-sm-8 col-sm-offset-2 no-padding content-white-bg buycoupon">
-        <Header title="入券" leftIcon="icon-angle-left" onLeftBtnClick={this.context.router.goBack} hide={utils.detector.isApp()}/>
+        <Header trasparent={trasparentHeader} title="入券" leftIcon="icon-angle-left" onLeftBtnClick={this.context.router.goBack} hide={utils.detector.isApp()}/>
         <Image className="coupon-img" src={imgSrc} quality={70} />
         <div className="product-info bottom-border bg-white col-xs-offset-1">
           <div className="row no-margin">
