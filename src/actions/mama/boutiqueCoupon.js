bc65563de2568fad35e4fb746a0fc3ff0d968425
wulei <crackwl@163.com>
2016-11-30 22:19:36 +0800

    add exchange action and reducer

--- src/actions/mama/boutiqueCoupon.js
+++ src/actions/mama/boutiqueCoupon.js
@@ -6,6 +6,8 @@ import qs from 'qs';
 export const names = {
   FETCH_MAMA_TRANCOUPON: 'FETCH_MAMA_TRANCOUPON',
   FETCH_MAMA_LEFT_TRANCOUPON: 'FETCH_MAMA_LEFT_TRANCOUPON',
+  FETCH_CAN_EXCHG_ORDERS: 'FETCH_CAN_EXCHG_ORDERS',
+  EXCHG_ORDER: 'EXCHG_ORDER',
 };
 
 export const fetchMamaTranCouponProfile = () => {
@@ -27,6 +29,34 @@ export const fetchMamaLeftTranCoupon = () => {
   return (dispatch) => {
     dispatch(action.request());
     return axios.get(`${constants.baseEndpoint}mama/trancoupon/list_mama_left_coupons`)
+      .then((resp) => {
+        dispatch(action.success(resp.data));
+      })
+      .catch((resp) => {
+        dispatch(action.failure(resp));
+      });
+  };
+};
+
+export const fetchCanExchgOrders = () => {
+  const action = createAction(names.FETCH_CAN_EXCHG_ORDERS);
+  return (dispatch) => {
+    dispatch(action.request());
+    return axios.get(`${constants.baseEndpoint}mama/exchgorder/list_can_exchg_orders`)
+      .then((resp) => {
+        dispatch(action.success(resp.data));
+      })
+      .catch((resp) => {
+        dispatch(action.failure(resp));
+      });
+  };
+};
+
+export const exchgOrder = (orderId, templateId, num) => {
+  const action = createAction(names.EXCHG_ORDER);
+  return (dispatch) => {
+    dispatch(action.request());
+    return axios.post(`${constants.baseEndpoint}mama/exchgorder/start_exchange`, { params: { order_id: orderId, exchg_template_id: templateId, coupon_num: num } })
       .then((resp) => {
         dispatch(action.success(resp.data));
       })
