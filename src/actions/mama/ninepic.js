ef3d7faabc13f273428b12180c28bbdeef649653
imeron <xiuqing.mei@xiaolu.so>
2017-02-28 15:27:53 +0800

    fix fetchNinePic

--- src/actions/mama/ninepic.js
+++ src/actions/mama/ninepic.js
@@ -2,16 +2,17 @@ import * as constants from 'constants';
 import axios from 'axios';
 import createAction from '../createAction';
 import qs from 'qs';
-import { parseParam2URIString } from 'utils/url';
+import urlUtils from 'utils/url';
 
 export const name = 'FETCH_NINE_PIC';
 
 const action = createAction(name);
 
 export const fetchNinePic = (params) => {
+  const queryString = urlUtils.parseParam2URIString(params || {});
   return (dispatch) => {
     dispatch(action.request());
-    return axios.get(`${constants.baseEndpointV1}pmt/ninepic?${parseParam2URIString(params)}`)
+    return axios.get(`${constants.baseEndpointV1}pmt/ninepic?${queryString}`)
       .then((resp) => {
         dispatch(action.success(resp.data));
       })
