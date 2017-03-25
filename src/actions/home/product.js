80c54ae853a3184dc30c96b638093f0be5899fb3
wulei <crackwl@163.com>
2017-03-25 20:50:31 +0800

    opt enter mamahome

--- src/actions/home/product.js
+++ src/actions/home/product.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 import _ from 'underscore';
 import createAction from '../createAction';
 
-export const name = 'FETCH_PRODUCT';
+export const productActionName = 'FETCH_PRODUCT';
 
 export const fetchProduct = (when, pageIndex, pageSize, cid, orderBy) => {
-  const action = createAction(name);
+  const action = createAction(productActionName);
   return (dispatch) => {
     dispatch(action.request({ when: when }));
     return axios.get(constants.baseEndpoint + 'modelproducts' + (when ? '/' + when : ''), { params: { page: pageIndex, page_size: pageSize, cid: cid, order_by: orderBy } })
@@ -22,14 +22,14 @@ export const fetchProduct = (when, pageIndex, pageSize, cid, orderBy) => {
 };
 
 export const resetProducts = () => {
-  const action = createAction(name);
+  const action = createAction(productActionName);
   return (dispatch) => {
     dispatch(action.reset());
   };
 };
 
 export const fetchVirtualProductDetails = (pageIndex, pageSize) => {
-  const action = createAction(name);
+  const action = createAction(productActionName);
   return (dispatch) => {
     dispatch(action.request());
     const params = { page: pageIndex, page_size: pageSize };
