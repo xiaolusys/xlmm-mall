07260b20b64f8410e6a8db454aecb873c25803a6
wulei <crackwl@163.com>
2017-03-26 18:07:37 +0800

    fix buycoupon list page bug

--- src/containers/Mama/BuyCoupon/List/index.js
+++ src/containers/Mama/BuyCoupon/List/index.js
@@ -1,41 +1,51 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import ReactDOM from 'react-dom';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import _ from 'underscore';
-import { InputHeader } from 'components/InputHeader';
-import { Image } from 'components/Image';
-import { Checkbox } from 'components/Checkbox';
-import { Input } from 'components/Input';
-import { Toast } from 'components/Toast';
-import { Popup } from 'components/Popup';
-import { If } from 'jsx-control-statements';
+import { Link } from 'react-router';
+import classnames from 'classnames';
 import * as utils from 'utils';
+import * as constants from 'constants';
+import { If } from 'jsx-control-statements';
+import { Loader } from 'components/Loader';
+import { InputHeader } from 'components/InputHeader';
+import { Timer } from 'components/Timer';
+import { CouponProduct } from 'components/CouponProduct';
+import { Image } from 'components/Image';
+import { ShopBag } from 'components/ShopBag';
+import { Favorite } from 'components/Favorite';
+import { BackTop } from 'components/BackTop';
+
 import * as mamaInfoAction from 'actions/mama/mamaInfo';
 import * as detailsAction from 'actions/home/product';
 import * as searchAction from 'actions/product/search';
-import * as plugins from 'plugins';
-
-import './index.scss';
 
 const actionCreators = _.extend(mamaInfoAction, detailsAction, searchAction);
 
+import './index.scss';
+
 @connect(
   state => ({
+    product: {
+      data: state.products.data,
+      isLoading: state.products.isLoading,
+      error: state.products.error,
+      success: state.products.success,
+    },
     mamaInfo: state.mamaInfo,
-    productDetails: state.products,
     search: state.searchProduct,
   }),
   dispatch => bindActionCreators(actionCreators, dispatch),
 )
 export default class TranCouponList extends Component {
   static propTypes = {
-    children: React.PropTypes.array,
     location: React.PropTypes.object,
     fetchVirtualProductDetails: React.PropTypes.func,
-    fetchMamaInfo: React.PropTypes.func,
+    resetProducts: React.PropTypes.func,
+    product: React.PropTypes.any,
     mamaInfo: React.PropTypes.any,
-    productDetails: React.PropTypes.object,
+    fetchMamaInfo: React.PropTypes.func,
     search: React.PropTypes.object,
     searchProduct: React.PropTypes.func,
     resetSearchProduct: React.PropTypes.func,
@@ -53,13 +63,17 @@ export default class TranCouponList extends Component {
   state = {
     hasMore: true,
     pageIndex: 0,
-    pageSize: 10,
+    pageSize: 20,
+    activeTab: 'default',
     sticky: false,
+    virtualProducts: [],
     searchFlag: false,
   }
 
   componentWillMount() {
     const { pageIndex, pageSize } = this.state;
+    const { cid } = this.props.location.query;
+    this.props.resetProducts();
     this.props.fetchVirtualProductDetails(pageIndex + 1, pageSize);
     this.props.fetchMamaInfo();
   }
@@ -69,34 +83,58 @@ export default class TranCouponList extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    const { mamaInfo, productDetails, search } = nextProps;
-
-    if (nextProps.isLoading) {
-      utils.ui.loadingSpinner.show();
-    } else if (!nextProps.isLoading) {
-      utils.ui.loadingSpinner.hide();
-    }
-
+    const mamaInfo = nextProps.mamaInfo.mamaInfo;
     let count = 0;
     let size = 0;
-    let data = null;
-    if (this.state.searchFlag) {
-      if (search.searchProduct.success) {
-        data = search.searchProduct.data;
-      }
-    } else {
-      if (productDetails.success) {
-        data = productDetails.data;
-      }
-    }
-
-    if (data) {
-      count = data.count;
-      size = data.results.length;
+    if (nextProps.product.success) {
+      count = nextProps.product.data.count;
+      size = nextProps.product.data.results.length;
       this.setState({ pageIndex: Math.round(size / this.state.pageSize) });
       this.setState({ hasMore: count > size });
-    }
 
+      const virtualProducts = [];
+      if (!this.state.searchFlag && mamaInfo.data && mamaInfo.data.length > 0
+          && nextProps.product.data.results && nextProps.product.data.results.length > 0) {
+        let sku = null;
+        for (let i = 0; i <= nextProps.product.data.results.length - 1; i++) {
+          const productDetails = nextProps.product.data.results[i];
+          for (let j = 0; j < productDetails.sku_info.length; j++) {
+            if (productDetails.sku_info[j].name.indexOf(mamaInfo.data[0].elite_level) >= 0) {
+              sku = productDetails.sku_info[j];
+              break;
+            }
+          }
+          const item = {
+            id: productDetails.id,
+            name: (productDetails.detail_content && sku) ? productDetails.detail_content.name + '/' + sku.name : '',
+            head_img: (productDetails && productDetails.detail_content) ? productDetails.detail_content.head_img : '',
+            agent_price: sku.agent_price,
+            lowest_std_sale_price: sku.std_sale_price,
+            elite_score: sku.elite_score,
+          };
+          virtualProducts.push(item);
+        }
+        this.setState({ virtualProducts: virtualProducts });
+      }
+      if (this.state.searchFlag && mamaInfo.data && mamaInfo.data.length > 0
+          && nextProps.search.searchProduct.data.results) {
+        if (nextProps.search.searchProduct.data.results.length > 0) {
+          for (let i = 0; i <= nextProps.search.searchProduct.data.results.length - 1; i++) {
+            const productDetails = nextProps.search.searchProduct.data.results[i];
+            const item = {
+              id: productDetails.id,
+              name: (productDetails.name) ? productDetails.name : '',
+              head_img: (productDetails) ? productDetails.head_img : '',
+              agent_price: '-',
+              lowest_std_sale_price: productDetails.lowest_std_sale_price,
+              elite_score: productDetails.elite_score,
+            };
+            virtualProducts.push(item);
+          }
+        }
+        this.setState({ virtualProducts: virtualProducts });
+      }
+    }
   }
 
   componentWillUnmount() {
@@ -104,64 +142,50 @@ export default class TranCouponList extends Component {
     this.setState({ pageIndex: 0 });
   }
 
-  onProductClick = (e) => {
-    const { index, modelid } = e.currentTarget.dataset;
-    // this.context.router.push('/product/details/' + modelid);
-    window.location.href = '/mall/buycoupon?index=' + index + '&modelid=' + modelid;
-  }
-
-  onCategoryClick = (e) => {
-    let { cid } = this.props.location.query;
-    if (!cid) {
-      cid = 0;
-    }
-    window.location.href = '/mall/product/categories' + (cid ? `?cid=${cid}` : '') + '&title=分类' + '&product_type=coupon';
+  onItemClick = (e) => {
+    const { modelid } = e.currentTarget.dataset;
+    window.location.href = '/mall/buycoupon?modelid=' + modelid;
   }
 
   onLeftBtnClick = (e) => {
     if (this.state.searchFlag) {
-      this.setState({ searchFlag: false, searchName: '' });
-      this.context.router.replace('/trancoupon/list');
+      this.setState({ searchFlag: false, searchName: '', pageIndex: 0 });
+      // this.context.router.replace('/trancoupon/list');
+      this.props.resetProducts();
+      this.props.fetchVirtualProductDetails(1, this.state.pageSize);
     } else {
       this.context.router.goBack();
     }
   }
-
   onInputChange = (e) => {
     const value = e.target.value;
     this.setState({ searchName: value });
   }
-
   onSearchClick = (e) => {
     if (this.state.searchName && this.state.searchName.length > 0) {
       this.props.resetSearchProduct();
       this.props.searchProduct(this.state.searchName, 1, 1, this.state.pageSize);
-      this.setState({ searchFlag: true });
+      this.setState({ searchFlag: true, pageIndex: 0 });
     }
   }
 
   onScroll = (e) => {
-    const { pageSize, pageIndex, searchFlag } = this.state;
-    const { fetchVirtualProductDetails, searchProduct } = this.props;
+    const { pageSize, pageIndex, activeTab } = this.state;
+    const { fetchVirtualProductDetails, product } = this.props;
+    const { cid } = this.props.location.query;
     const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     const documentHeight = utils.dom.documnetHeight();
     const windowHeight = utils.dom.windowHeight();
-    const tabsOffsetTop = utils.dom.offsetTop('.buycoupon-container');
 
     if (scrollTop === documentHeight - windowHeight && this.state.hasMore) {
       if (this.state.searchFlag && !this.props.search.searchProduct.isLoading) {
         this.props.searchProduct(this.state.searchName, 1, pageIndex + 1, pageSize);
       }
-      if (!this.state.searchFlag && !this.props.productDetails.isLoading) {
+      if (!this.state.searchFlag && !this.props.product.isLoading) {
         this.props.fetchVirtualProductDetails(pageIndex + 1, pageSize);
       }
     }
 
-    // if (scrollTop > tabsOffsetTop) {
-    //   this.setState({ sticky: true });
-    // } else {
-    //   this.setState({ sticky: false });
-    // }
   }
 
   addScrollListener = () => {
@@ -172,91 +196,30 @@ export default class TranCouponList extends Component {
     window.removeEventListener('scroll', this.onScroll);
   }
 
-  renderProduct = (product, index) => {
-    const mamaInfo = this.props.mamaInfo.mamaInfo;
-    const productDetails = product;
-    const imgSrc = (productDetails && productDetails.detail_content) ? productDetails.detail_content.head_img : '';
-    let sku = null;
-
-    if (this.state.searchFlag) {
-      return (
-      <li key={index} className="col-xs-6 product-item bottom-border" data-index={index} data-modelid={product.id} onClick={this.onProductClick}>
-        <Image className="coupon-img" src={product.head_img} quality={70} />
-        <div className="product-info bg-white">
-          <div className="row no-margin">
-            <p className="no-padding no-wrap font-xs">{product.name }</p>
-          </div>
-          <div className="row no-margin">
-            <p className="no-wrap">
-              <span className="font-xs">{'￥'}</span>
-              <span className="font-grey">/</span>
-              <span className="font-xs font-grey-light text-line-through">{'￥' + (product.lowest_std_sale_price)}</span>
-              <span className="font-grey font-xs" style={{ paddingLeft: '8px' }}>{(product.elite_score) ? '积分' + product.elite_score : ''}</span>
-            </p>
-          </div>
-        </div>
-      </li>
-    );
-    }
-
-    for (let i = 0; i < productDetails.sku_info.length; i++) {
-      if (productDetails.sku_info[i].name.indexOf(mamaInfo.data[0].elite_level) >= 0) {
-        sku = productDetails.sku_info[i];
-        break;
-      }
-    }
-
-    return (
-      <li key={index} className="col-xs-6 product-item bottom-border" data-index={index} data-modelid={product.id} onClick={this.onProductClick}>
-        <Image className="coupon-img" src={imgSrc} quality={70} />
-        <div className="product-info bg-white">
-          <div className="row no-margin">
-            <p className="no-padding no-wrap font-xs">{(productDetails.detail_content && sku) ? productDetails.detail_content.name + '/' + sku.name : '' }</p>
-          </div>
-          <div className="row no-margin">
-            <p className="no-wrap">
-              <span className="font-xs">{'￥' + (sku ? sku.agent_price : '')}</span>
-              <span className="font-grey">/</span>
-              <span className="font-xs font-grey-light text-line-through">{'￥' + (sku ? sku.std_sale_price : '')}</span>
-              <span className="font-grey font-xs" style={{ paddingLeft: '8px' }}>{(sku) ? '积分' + sku.elite_score : ''}</span>
-            </p>
-          </div>
-        </div>
-      </li>
-    );
-  }
-
   render() {
-    const { productDetails, search } = this.props;
-    const mamaInfo = this.props.mamaInfo.mamaInfo;
-    const trasparentHeader = false;
-    const { sticky } = this.state;
+    const { product, search } = this.props;
+    const { title } = this.props.location.query;
+    const products = this.state.virtualProducts || [];
+    const hasHeader = !utils.detector.isApp();
+    const { activeTab, sticky } = this.state;
 
     return (
-      <div className=" content-white-bg buycouponlist">
+      <div className="buycoupon-list">
         <InputHeader placeholder=" 输入查询的商品" onInputChange={this.onInputChange} leftIcon="icon-angle-left" onLeftBtnClick={this.onLeftBtnClick} rightText="搜索" onRightBtnClick={this.onSearchClick} />
-        <div className="content no-padding">
-          <div className={'buycoupon-container ' + (sticky ? 'sticky ' : '') }>
-          </div>
-          <div className="return-list-p no-padding" >
-            <If condition={productDetails.success && productDetails.data && mamaInfo.success && mamaInfo.data && !this.state.searchFlag}>
-              <ul className="no-padding">
-              {productDetails.data.results.map((item, index) => this.renderProduct(item, index))
-              }
-              </ul>
-            </If>
-            <If condition={search.searchProduct.success && search.searchProduct.data && (search.searchProduct.data.count > 0) && mamaInfo.success && mamaInfo.data && this.state.searchFlag}>
-              <ul className="no-padding">
-              {search.searchProduct.data.results.map((item, index) => this.renderProduct(item, index))
-              }
-              </ul>
-            </If>
+        <div className="content content-white-bg">
+          <div className="product-list-p" >
+          <div className="margin-top-xxs" ></div>
+            {products.map((item) => {
+              return <CouponProduct key={item.id} product={item} onItemClick = {this.onItemClick} />;
+            })}
           </div>
           <If condition={search.searchProduct.success && search.searchProduct.data && (search.searchProduct.data.count === 0) && this.state.searchFlag}>
-              <div className="empty-search">
-                <p>抱歉，商城没有您要查询的商品！</p>
-              </div>
-            </If>
+            <div className="empty-search">
+              <p style={{ paddingTop: '8px', paddingDown: '8px', textAlign: 'center' }}>抱歉，商城没有您要查询的商品！</p>
+            </div>
+          </If>
+          {product.isLoading ? <Loader/> : null}
+          <BackTop />
         </div>
       </div>
     );
