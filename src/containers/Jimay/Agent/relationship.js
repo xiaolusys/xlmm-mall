79cc520a9a5dc41a4072caf6e2b505ec185efc88
imeron <xiuqing.mei@xiaolu.so>
2017-06-19 22:27:31 +0800

    fix jimay relationship

--- src/containers/Jimay/Agent/relationship.js
+++ src/containers/Jimay/Agent/relationship.js
@@ -83,7 +83,7 @@ export default class JimayAgentRelShip extends Component {
                 <div className="col-xs-9 no-padding">
                   <p>
                     <span className="font-md font-grey">{ parentAgent.name }</span>
-                    <a href="tel:13922897782" className="font-md font-orange font-weight-600 margin-right-sm pull-right">{ parentAgent.mobile }</a>
+                    <a href={ 'tel:' + parentAgent.mobile} className="font-md font-orange font-weight-600 margin-right-sm pull-right">{ parentAgent.mobile }</a>
                   </p>
                 </div>
               </li>
@@ -105,7 +105,7 @@ export default class JimayAgentRelShip extends Component {
                     <div className="col-xs-9 no-padding">
                       <p>
                         <span className="font-md font-grey">{ item.name }</span>
-                        <a href="tel:13922897782" className="font-md font-orange font-weight-600 margin-right-sm pull-right">{ item.mobile }</a>
+                        <a href={ 'tel:' + item.mobile} className="font-md font-orange font-weight-600 margin-right-sm pull-right">{ item.mobile }</a>
                       </p>
                     </div>
                   </li>
